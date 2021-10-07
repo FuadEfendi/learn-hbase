8062ae56a9d9b966369693851c6a202c96a77ef1
Fuad Efendi <fuad@efendi.ca>
2021-10-07 06:35:08 -0400

    initial release

---
+++ src/main/java/ca/tokenizer/learn/learnhbase/LearnHbaseApplication.java
@@ -0,0 +1,13 @@
+package ca.tokenizer.learn.learnhbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnHbaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnHbaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ca/tokenizer/learn/learnhbase/LearnHbaseApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.tokenizer.learn.learnhbase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnHbaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
